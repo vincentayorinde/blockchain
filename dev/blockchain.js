5ba30aed4ed5a8b651fc388e64870cecd77dbcaf
vincentayorinde <vincentayorinde@yahoo.com>
2020-03-16 06:51:51 +0100

    setup hash test hashing with new Block

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,3 +1,5 @@
+const sha256 = require('sha256');
+
 function Blockchain() {
     this.chain = [];
     this.pendingTransactions = [];
@@ -37,8 +39,15 @@ Blockchain.prototype.createNewTransaction = function(
     return this.getLastBlock()['index'] + 1;
 };
 
-Blockchain.prototype.hashBlock = function (blockData) {
-    
-}
+Blockchain.prototype.hashBlock = function(
+    previousBlockHash,
+    currentBlockData,
+    nonce
+) {
+    const dataAsString =
+        previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
+    const hash = sha256(dataAsString);
+    return hash;
+};
 
 module.exports = Blockchain;
--- dev/test.js
+++ dev/test.js
@@ -2,46 +2,68 @@ const Blockchain = require('./blockchain');
 
 const bitcoin = new Blockchain();
 
-bitcoin.createNewBlock(
-    2345,
-    'flasdhfakfjdkjfkdskdjhfalksdhfasdf',
-    'fakdsfalksdhflakshdfkljashdflka'
-);
+const previousBlockHash = 'flaskdjhflkjahsdflashdflkuahsdf';
+const currentBlockData = [
+    {
+        amount: 20,
+        sender: 'NEWfshdlkjfahsljdfhsldfj',
+        recipient: 'sdfljsdkljfhsdkjfhsdjf'
+    },
+    {
+        amount: 40,
+        sender: 'sdflkjsdlkfjlskdjflkjsdf',
+        recipient: 'lkjsdflkjslkdjflksdf'
+    },
+    {
+        amount: 5000,
+        sender: 'lkashdfkljhwehrjklfelwf',
+        recipient: 'flsdjflksjdflkshdfliwehl'
+    }
+];
+const nonce = 100;
 
-bitcoin.createNewTransaction(
-    100,
-    'BENfsdiflasdjkfhkkhkjhjhgsdjfhsdjk',
-    'FRANdfskldjflksdfipisduofsdkfjslkd'
-);
+console.log(bitcoin.hashBlock(previousBlockHash, currentBlockData, nonce));
 
-bitcoin.createNewBlock(
-    7879,
-    'sdfjhaksjdhflaksjhdflkjajshdf',
-    'lsjdfljsdlfkjsdjkfhskjf'
-);
+// bitcoin.createNewBlock(
+//     2345,
+//     'flasdhfakfjdkjfkdskdjhfalksdhfasdf',
+//     'fakdsfalksdhflakshdfkljashdflka'
+// );
 
-bitcoin.createNewTransaction(
-    50,
-    'JOEfsdiflasdjkfhkkhkjhjhgsdjfhsdjk',
-    'DANdfskldjflksdfipisduofsdkfjslkd'
-);
+// bitcoin.createNewTransaction(
+//     100,
+//     'BENfsdiflasdjkfhkkhkjhjhgsdjfhsdjk',
+//     'FRANdfskldjflksdfipisduofsdkfjslkd'
+// );
 
-bitcoin.createNewTransaction(
-    200,
-    'BOLAfsdiflasdjkfhkkhkjhjhgsdjfhsdjk',
-    'TAYOdfskldjflksdfipisduofsdkfjslkd'
-);
+// bitcoin.createNewBlock(
+//     7879,
+//     'sdfjhaksjdhflaksjhdflkjajshdf',
+//     'lsjdfljsdlfkjsdjkfhskjf'
+// );
 
-bitcoin.createNewTransaction(
-    2500,
-    'INTELfsdiflasdjkfhkkhkjhjhgsdjfhsdjk',
-    'VINCENTdfskldjflksdfipisduofsdkfjslkd'
-);
+// bitcoin.createNewTransaction(
+//     50,
+//     'JOEfsdiflasdjkfhkkhkjhjhgsdjfhsdjk',
+//     'DANdfskldjflksdfipisduofsdkfjslkd'
+// );
 
-bitcoin.createNewBlock(
-    789,
-    'lsdfjalkjsdfjkahsdkjfhkajshdfkjhas',
-    'kjhaskdjfhkjsdhfkjadshkjfhasjdasdf'
-);
+// bitcoin.createNewTransaction(
+//     200,
+//     'BOLAfsdiflasdjkfhkkhkjhjhgsdjfhsdjk',
+//     'TAYOdfskldjflksdfipisduofsdkfjslkd'
+// );
 
-console.log(bitcoin.chain[2]);
+// bitcoin.createNewTransaction(
+//     2500,
+//     'INTELfsdiflasdjkfhkkhkjhjhgsdjfhsdjk',
+//     'VINCENTdfskldjflksdfipisduofsdkfjslkd'
+// );
+
+// bitcoin.createNewBlock(
+//     789,
+//     'lsdfjalkjsdfjkahsdkjfhkajshdfkjhas',
+//     'kjhaskdjfhkjsdhfkjadshkjfhasjdasdf'
+// );
+
+// console.log(bitcoin);
