1fdf2c9f63debfa5b9eec543a468510160cf6497
vincentayorinde <vincentayorinde@yahoo.com>
2020-03-16 07:40:01 +0100

    create proofOfwork function test to ensure functionality

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -50,4 +50,18 @@ Blockchain.prototype.hashBlock = function(
     return hash;
 };
 
+Blockchain.prototype.proofOfWork = function(
+    previousBlockHash,
+    currentBlockData
+) {
+    let nonce = 0;
+    let hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+    while (hash.substring(0, 4) !== '0000') {
+        nonce++;
+        hash = this.hashBlock(previousBlockHash, currentBlockData, nonce);
+        console.log('the nonce:', nonce, 'hash is:', hash);
+    }
+    return nonce;
+};
+
 module.exports = Blockchain;
